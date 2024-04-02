8b45943995015d85cf73e94127f2543328a9c0e8
Roman <151219635+M1soKanwa@users.noreply.github.com>
2024-04-02 22:45:10 +0200

    How require works

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 'use strict';
 
- const {MyMath} = require('./MyMath.js');
- const {Component} = require('./Component.js');
+ /*
 
- const sum2n2 = MyMath.sum(2,2);
- console.log(sum2n2);
- const multy3n5 = MyMath.multy(3,5);
- console.log(multy3n5);
+ require (path)
 
- const component = new Component();
- console.log(component);
+ how require works : 
+
+ resolving -->> loading -->> wrappening -->> evaluation -->> caching 
+
+ RESOLVING :
+    1) Core modules 
+    2) File
+    *.js  // *.json
+    3) 
+        3. 1) package json -> *main*
+        3. 2) index.js || index.json
+    4) node_modules     
+    5) throw new Error().
+ */
