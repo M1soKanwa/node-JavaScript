c1f0a4f999d74a4ae8b680042025754ed5f27479
Roman <151219635+M1soKanwa@users.noreply.github.com>
2024-04-03 15:13:48 +0200

    Add lodash + package.json

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 'use strict';
 
- /*
-
- require (path)
-
- how require works : 
-
- resolving -->> loading -->> wrappening -->> evaluation -->> caching 
-
- RESOLVING :
-    1) Core modules 
-    2) File
-    *.js  // *.json
-    3) 
-        3. 1) package json -> *main*
-        3. 2) index.js || index.json
-    4) node_modules     
-    5) throw new Error().
- */
+console.log('Hello world');
